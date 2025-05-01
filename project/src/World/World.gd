8bbe1c14507be716254603197922f33a54342956
GameYammer <GameYammer@gmail.com>
2025-05-01 12:49:01 -0700

    Added button to add smoke to world scene.

--- project/src/World/World.gd
+++ project/src/World/World.gd
@@ -34,3 +34,33 @@ func _on_button_add_raptor_pressed() -> void:
 	)
 	end = Time.get_ticks_usec()
 	print("Add to world time: %s usec" % [end - start])
+
+
+func _on_button_add_smoke_pressed() -> void:
+	var start := 0
+	var end := 0
+	print("===========================================")
+
+	# Load scene file
+	start = Time.get_ticks_usec()
+	var scene : PackedScene = ResourceCache.get_resource("res://src/Effects/BlackSmokeConstant/BlackSmokeConstant.tscn")
+	end = Time.get_ticks_usec()
+	print("Load tscn file time: %s usec" % [end - start])
+
+	# Instance smoke scene
+	start = Time.get_ticks_usec()
+	var smoke : GPUParticles3D = scene.instantiate()
+	end = Time.get_ticks_usec()
+	print("Create instance time: %s usec" % [end - start])
+
+	# Add the smoke to the world at a random position
+	start = Time.get_ticks_usec()
+	self.add_child(smoke)
+	const r := 10.0
+	smoke.transform.origin = Vector3(
+		randf_range(-r, r),
+		0.0,
+		randf_range(-r, r),
+	)
+	end = Time.get_ticks_usec()
+	print("Add to world time: %s usec" % [end - start])
